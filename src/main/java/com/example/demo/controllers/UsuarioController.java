86bed3d9ff0bbef28797b10d770525cb10e6e96c
zulma18 <zuzuguevara25@gmail.com>
2024-08-03 22:42:15 -0600

    ej
    prueba

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuario Prueba")
 public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
